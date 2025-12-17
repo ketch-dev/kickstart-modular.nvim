61ee961f804d74dcdd4e44f832d7988bd563a2ec
Ilya Marchik <ketch.dev@gmail.com>
2025-12-17 17:44:34 +0100

    [nvim] setup kickstart indent line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,10 +1,14 @@
+-- ========== Add indentation guides even on blank lines ==========
+
 return {
-  { -- Add indentation guides even on blank lines
+  {
     cond = not vim.g.vscode,
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = { char = '╎' },
+    },
   },
 }
