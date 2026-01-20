0ddbbab05e044142f59d78418d84c4a081ebd6f1
Ilya Marchik <ketch.dev@gmail.com>
2026-01-20 09:40:50 +0100

    fix neo-tree config

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,60 +15,62 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    hide_root_node = true,
+    retain_hidden_root_indent = false,
     close_if_last_window = true,
-    sources = { "filesystem", "buffers", "git_status" },
+    sources = { 'filesystem', 'buffers', 'git_status' },
+    window = {
+      mappings = {
+        ['<space>'] = 'none',
+
+        ['h'] = 'none',
+        ['j'] = 'none',
+        ['k'] = 'none',
+        ['l'] = 'none',
+
+        ['<left>'] = 'close_node',
+        ['<down>'] = 'move_down',
+        ['<up>'] = 'move_up',
+        ['<right>'] = 'open',
+
+        ['n'] = 'rename',
+        ['y'] = 'copy_to_clipboard',
+        ['s'] = 'open_vsplit',
+        ['k'] = 'close_all_nodes',
+        ['K'] = 'expand_all_nodes',
+
+        -- ['<C-p>'] = 'prev_source',
+        -- ['<C-n>'] = 'next_source',
+
+        -- Restore original functions elsewhere (e.g., <Leader> keys)
+        -- ['<Leader>r'] = 'refresh', -- Original 'r' (refresh)
+        -- ['<Leader>s'] = 'split', -- Original 's' (split)
+        -- ['<Leader>f'] = 'filter', -- Original 'f' (filter)
+        -- ['<Leader>t'] = 'open_tab', -- Original 't' (open in tab)
+      },
+    },
     filesystem = {
-      window = {
-        mappings = {
-          ['<space>'] = 'none',
-  
-          ['h'] = 'none',
-          ['j'] = 'none',
-          ['k'] = 'none',
-          ['l'] = 'none',
-  
-          ['<left>'] = 'close_node',
-          ['<bottom>'] = 'move_down',
-          ['<top>'] = 'move_up',
-          ['<right>'] = 'open',
-
-          -- ['<C-p>'] = 'prev_source',
-          -- ['<C-n>'] = 'next_source',
-
-          -- Restore original functions elsewhere (e.g., <Leader> keys)
-          -- ['<Leader>r'] = 'refresh', -- Original 'r' (refresh)
-          -- ['<Leader>s'] = 'split', -- Original 's' (split)
-          -- ['<Leader>f'] = 'filter', -- Original 'f' (filter)
-          -- ['<Leader>t'] = 'open_tab', -- Original 't' (open in tab)
-
-          ['n'] = 'rename',
-          ['y'] = 'copy_to_clipboard',
-          ['s'] = 'open_vsplit',
-          ['k'] = 'close_all_nodes',
-          ['K'] = 'expand_all_nodes',
+      hijack_netrw_behavior = 'open_default',
+      group_empty_dirs = true,
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          'node_modules',
+          '.git',
         },
       },
-      hide_root_node = true,
-      filesystem = {
-        hijack_netrw_behavior = 'open_default',
-        group_empty_dirs = true,
-        follow_current_file = { enabled = true },
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_by_name = {
-            'node_modules',
-            '.git',
-          },
-        },
-      },
-      default_component_configs = {
-        icon = {
-          folder_closed = '',
-          folder_open = '',
-          folder_empty = '',
-          folder_empty_open = '',
-        },
+    },
+    default_component_configs = {
+      icon = {
+        folder_closed = '',
+        folder_open = '',
+        folder_empty = '',
+        folder_empty_open = '',
       },
     },
   },
