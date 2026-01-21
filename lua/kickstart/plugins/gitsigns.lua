6de48a9f21d466f0b656ad407c36b3a1aeed78f6
Ilya Marchik <ketch.dev@gmail.com>
2026-01-21 15:25:31 +0100

    enable git blame in gitsigns

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -18,13 +18,20 @@ return {
     'lewis6991/gitsigns.nvim',
     cond = not vim.g.vscode,
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'right_align',
+        delay = 300,
       },
+
+      -- signs = {
+      --   add = { text = '+' },
+      --   change = { text = '~' },
+      --   delete = { text = '_' },
+      --   topdelete = { text = '‾' },
+      --   changedelete = { text = '~' },
+      -- },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
