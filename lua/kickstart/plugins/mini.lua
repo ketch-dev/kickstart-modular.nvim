278ec1390452756c7146b3ed99894f57bb61e4f4
Ilya Marchik <ketch.dev@gmail.com>
2026-01-21 16:52:30 +0100

    don't load mini.files in vscode env

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -60,47 +60,49 @@ return {
       end
 
       -- Setup mini.files (file explorer)
-      require('mini.files').setup {
-        cond = not vim.g.vscode,
-        mappings = {
-          close = 'q',
-          go_in = '<right>',
-          go_in_plus = '<CR>',
-          go_out = '<left>',
-          -- go_out_plus = "H",
-          synchronize = '=', -- Save changes
-        },
-        -- windows = {
-        --   preview = true,     -- Show preview of file under cursor
-        --   width_focus = 30,
-        --   width_nofocus = 15,
-        -- },
-      }
+      if not vim.g.vscode then
+        require('mini.files').setup {
+          cond = not vim.g.vscode,
+          mappings = {
+            close = 'q',
+            go_in = '<right>',
+            go_in_plus = '<CR>',
+            go_out = '<left>',
+            -- go_out_plus = "H",
+            synchronize = '=', -- Save changes
+          },
+          -- windows = {
+          --   preview = true,     -- Show preview of file under cursor
+          --   width_focus = 30,
+          --   width_nofocus = 15,
+          -- },
+        }
 
-      -- This opens it at the directory of the current buffer (or cwd if no file)
-      vim.keymap.set('n', '-', function()
-        local miniFiles = require 'mini.files'
-        local buf_path = vim.api.nvim_buf_get_name(0)
+        -- This opens it at the directory of the current buffer (or cwd if no file)
+        vim.keymap.set('n', '-', function()
+          local miniFiles = require 'mini.files'
+          local buf_path = vim.api.nvim_buf_get_name(0)
 
-        -- If the explorer is already open, close it
-        if miniFiles.close() then
-          return
-        end
+          -- If the explorer is already open, close it
+          if miniFiles.close() then
+            return
+          end
 
-        -- If we are in a file, open it (this focuses the file)
-        if buf_path ~= '' then
-          -- miniFiles.open(buf_path, true)
-          miniFiles.open(buf_path)
-        -- Ideally, we would 'reveal' the path from root here, but mini.files
-        -- truncates history by design.
-        -- Workaround: You can map a key inside mini.files to "Go to Root"
-        -- or just manually press 'h' until you hit root.
-        else
-          -- If no file, open CWD
-          -- miniFiles.open(nil, true) -- Open at cwd, focused
-          miniFiles.open(vim.uv.cwd())
-        end
-      end, { desc = 'Open mini.files' })
+          -- If we are in a file, open it (this focuses the file)
+          if buf_path ~= '' then
+            -- miniFiles.open(buf_path, true)
+            miniFiles.open(buf_path)
+          -- Ideally, we would 'reveal' the path from root here, but mini.files
+          -- truncates history by design.
+          -- Workaround: You can map a key inside mini.files to "Go to Root"
+          -- or just manually press 'h' until you hit root.
+          else
+            -- If no file, open CWD
+            -- miniFiles.open(nil, true) -- Open at cwd, focused
+            miniFiles.open(vim.uv.cwd())
+          end
+        end, { desc = 'Open mini.files' })
+      end
     end,
   },
 }
