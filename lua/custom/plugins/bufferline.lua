a62de68b64d2fac050af7977bafa490646fc1b94
Ilya Marchik <ketch.dev@gmail.com>
2026-02-10 11:16:17 +0100

    [bufferline] display tab number instead of it's focused buffer name

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -10,7 +10,13 @@ return {
       options = {
         separator_style = 'slant',
         mode = 'tabs',
+        name_formatter = function(tab)
+          local tabs = vim.api.nvim_list_tabpages()
+          local index = vim.fn.index(tabs, tab.tabnr)
+          return tostring(index + 1)
+        end,
         always_show_bufferline = false,
+        show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
         middle_mouse_command = function(id)
