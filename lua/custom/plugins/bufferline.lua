373a27faf7fe63be30a1bad6966baa3e498d598c
Ilya Marchik <ketch.dev@gmail.com>
2026-02-10 11:47:52 +0100

    [bufferline] display add tab button

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -6,6 +6,21 @@ return {
     cond = not vim.g.vscode,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     version = '*',
+    init = function()
+      _G.__bufferline_new_tab = function()
+        local function focus_last_tab()
+          local tabs = vim.api.nvim_list_tabpages()
+          local last = tabs[#tabs]
+          if last and vim.api.nvim_tabpage_is_valid(last) then
+            vim.api.nvim_set_current_tabpage(last)
+          end
+        end
+
+        vim.cmd 'tabnew'
+        vim.schedule(focus_last_tab)
+        vim.defer_fn(focus_last_tab, 10)
+      end
+    end,
     opts = {
       options = {
         separator_style = 'slant',
@@ -15,7 +30,7 @@ return {
           local index = vim.fn.index(tabs, tab.tabnr)
           return tostring(index + 1)
         end,
-        always_show_bufferline = false,
+        always_show_bufferline = true,
         show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
@@ -29,5 +44,20 @@ return {
         end,
       },
     },
+    config = function(_, opts)
+      require('bufferline').setup(opts)
+
+      if not _G.__bufferline_plus_after_tabs_wrapped then
+        local original = _G.nvim_bufferline
+        _G.nvim_bufferline = function()
+          local line, segments = original()
+          if type(line) == 'string' and not line:find('v:lua.__bufferline_new_tab', 1, true) then
+            line = line:gsub('%%=', '%%T%%@v:lua.__bufferline_new_tab@ + %%T%%=', 1)
+          end
+          return line, segments
+        end
+        _G.__bufferline_plus_after_tabs_wrapped = true
+      end
+    end,
   },
 }
