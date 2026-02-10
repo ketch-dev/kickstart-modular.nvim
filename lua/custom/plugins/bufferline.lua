26c9d683a91e58bdf7a7efc3930b9338d13222c4
Ilya Marchik <ketch.dev@gmail.com>
2026-02-10 10:18:03 +0100

    close bufferline tabs via middle mouse click

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,16 +1,27 @@
 -- ========== Show Tabs ==========
 
 return {
-  'akinsho/bufferline.nvim',
-  cond = not vim.g.vscode,
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  version = '*',
-  opts = {
-    options = {
-      mode = 'tabs',
-      always_show_bufferline = false,
+  {
+    'akinsho/bufferline.nvim',
+    cond = not vim.g.vscode,
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    version = '*',
+    opts = {
+      options = {
+        separator_style = 'slant',
+        mode = 'tabs',
+        always_show_bufferline = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        middle_mouse_command = function(id)
+          local close = require('bufferline.config').options.close_command
+          if type(close) == 'function' then
+            pcall(close, id)
+          elseif type(close) == 'string' then
+            pcall(vim.cmd, string.format(close, id))
+          end
+        end,
+      },
     },
   },
 }
