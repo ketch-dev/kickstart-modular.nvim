b5d352505c46cea129010caf43783e37a5ed4541
Ilya Marchik <ketch.dev@gmail.com>
2026-04-08 17:58:03 +0200

    add tabs/spaces config

--- lua/options.lua
+++ lua/options.lua
@@ -20,6 +20,15 @@ vim.opt.foldlevelstart = 99
 vim.opt.foldcolumn = '0'
 -------------------------------------------------------------------------------
 
+-- ========== Tabs/Spaces ==========
+vim.opt.tabstop = 4 -- tabwidth
+vim.opt.shiftwidth = 4 -- indent width
+vim.opt.softtabstop = 4 -- soft tab stop not tabs on tab/backspace
+vim.opt.expandtab = false -- use spaces instead of tabs
+vim.opt.smartindent = true -- smart auto-indent
+vim.opt.autoindent = true -- copy indent from current line
+-------------------------------------------------------------------------------
+
 -- ========== Wrap/Break ==========
 vim.opt.wrap = true
 vim.opt.linebreak = true -- Break at word boundaries
