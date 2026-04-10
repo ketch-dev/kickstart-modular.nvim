e90a6e5e38d18b666c1c0c3e52310cfa5d6e39be
Ilya Marchik <ketch.dev@gmail.com>
2026-04-10 09:17:05 +0200

    install kanagawa

---
+++ lua/custom/plugins/kanagawa.lua
@@ -0,0 +1,24 @@
+-- ========== Colorscheme ==========
+
+return {
+  {
+    'rebelot/kanagawa.nvim',
+    cond = not vim.g.vscode,
+    priority = 1000,
+    config = function()
+      require('kanagawa').setup {
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = 'none',
+              },
+            },
+          },
+        },
+      }
+
+      vim.cmd.colorscheme 'kanagawa-dragon'
+    end,
+  },
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -2,7 +2,7 @@ require('lazy').setup({
   -- require 'custom.plugins.themes.github-theme',
   -- require 'custom.plugins.themes.vscode',
   -- require 'custom.plugins.themes.vscode-syntax-blend',
-  require 'kickstart.plugins.tokyonight',
+  require 'custom.plugins.kanagawa',
 
   require 'custom.plugins.flash',
   require 'custom.plugins.bufferline',
