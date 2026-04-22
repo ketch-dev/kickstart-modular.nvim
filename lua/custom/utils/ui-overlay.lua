48deaf078ceca77c4552860020b88e498ac75f32
Ilya Marchik <ketch.dev@gmail.com>
2026-04-22 10:03:55 +0200

    [ui-overlay] group blocks

--- lua/custom/utils/ui-overlay.lua
+++ lua/custom/utils/ui-overlay.lua
@@ -15,15 +15,23 @@ function M.apply()
   local cursor_line_sign = vim.api.nvim_get_hl(0, { name = 'CursorLineSign', link = false })
   local cursor_line_fold = vim.api.nvim_get_hl(0, { name = 'CursorLineFold', link = false })
 
-  vim.api.nvim_set_hl(0, 'NormalNC', vim.tbl_extend('force', normal_nc, { bg = normal.bg }))
+  -- ========== [Gutter] Sign Column ==========
+  vim.api.nvim_set_hl(0, 'SignColumn', vim.tbl_extend('force', sign_column, { bg = normal.bg }))
+  vim.api.nvim_set_hl(0, 'CursorLineSign', vim.tbl_extend('force', cursor_line_sign, { bg = normal.bg }))
+  ---------------------------------------------------------------------------------
+
+  -- ========== [Gutter] Line Number ==========
   vim.api.nvim_set_hl(0, 'LineNr', vim.tbl_extend('force', line_nr, { bg = normal.bg }))
   vim.api.nvim_set_hl(0, 'CursorLineNr', vim.tbl_extend('force', cursor_line_nr, { bg = normal.bg, fg = '#bbbbbb' }))
-  vim.api.nvim_set_hl(0, 'SignColumn', vim.tbl_extend('force', sign_column, { bg = normal.bg }))
-  vim.api.nvim_set_hl(0, 'Folded', vim.tbl_extend('force', folded, { bg = '#1b1b1b', fg = '#2f2f2f', italic = true }))
+  ---------------------------------------------------------------------------------
+
+  -- ========== [Gutter] Fold ==========
   vim.api.nvim_set_hl(0, 'FoldColumn', vim.tbl_extend('force', fold_column, { bg = normal.bg }))
-  vim.api.nvim_set_hl(0, 'CursorLineSign', vim.tbl_extend('force', cursor_line_sign, { bg = normal.bg }))
   vim.api.nvim_set_hl(0, 'CursorLineFold', vim.tbl_extend('force', cursor_line_fold, { bg = normal.bg }))
-  -------------------------------------------------------------------------------
+  ---------------------------------------------------------------------------------
+
+  vim.api.nvim_set_hl(0, 'Folded', vim.tbl_extend('force', folded, { bg = '#1b1b1b', fg = '#2f2f2f', italic = true })) -- folded line
+  vim.api.nvim_set_hl(0, 'NormalNC', vim.tbl_extend('force', normal_nc, { bg = normal.bg })) -- unfocused buffer
 end
 
 function M.setup()
