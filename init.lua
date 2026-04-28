494c6aa88d17264ee9f9afcb1e176123ee44efef
Ilya Marchik <ketch.dev@gmail.com>
2026-04-28 17:36:20 +0200

    remove center-buffer implementation with left and right panes

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 require 'options'
 require 'notify-vscode'
--- require 'custom.center-buffer'
 require 'keymaps'
 require 'lazy-bootstrap'
 require 'lazy-plugins'
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,7 +26,6 @@ vim.diagnostic.config {
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'prev [d]iagnostic' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'next [d]iagnostic' })
 vim.keymap.set('n', '<leader>sd', function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end, { desc = '[d]iagnostic' })
-vim.keymap.set('n', '<leader>sc', '<cmd>CenterBufferToggle<CR>', { desc = '[c]enter buffer' })
 -------------------------------------------------------------------------------
 
 -- ========== Disable hjkl ==========
