40aa9c96ab19ede0e05005d06c2083a9efd0351d
Ilya Marchik <ketch.dev@gmail.com>
2026-05-04 17:21:16 +0200

    add custom focus on dap hover in debug

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -22,6 +22,43 @@ local function continue_or_run_first_config()
   end
 end
 
+local dap_hover_view
+local dap_hover_autocmd
+
+local function close_dap_hover(view)
+  if dap_hover_autocmd then
+    pcall(vim.api.nvim_del_autocmd, dap_hover_autocmd)
+    dap_hover_autocmd = nil
+  end
+
+  if view and view.close then view.close() end
+  if dap_hover_view == view then dap_hover_view = nil end
+end
+
+local function focus_or_open_dap_hover()
+  if dap_hover_view and dap_hover_view.win and vim.api.nvim_win_is_valid(dap_hover_view.win) then
+    vim.api.nvim_set_current_win(dap_hover_view.win)
+    return
+  end
+
+  local source_win = vim.api.nvim_get_current_win()
+  local source_buf = vim.api.nvim_get_current_buf()
+  local view = require('dap.ui.widgets').hover()
+  dap_hover_view = view
+
+  if vim.api.nvim_win_is_valid(source_win) then vim.api.nvim_set_current_win(source_win) end
+
+  vim.schedule(function()
+    if dap_hover_view ~= view or not vim.api.nvim_buf_is_valid(source_buf) then return end
+
+    if dap_hover_autocmd then pcall(vim.api.nvim_del_autocmd, dap_hover_autocmd) end
+    dap_hover_autocmd = vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+      buffer = source_buf,
+      callback = function() close_dap_hover(view) end,
+    })
+  end)
+end
+
 ---@module 'lazy'
 ---@type LazySpec
 return {
@@ -54,7 +91,7 @@ return {
     },
     { '<leader>dc', continue_or_run_first_config, desc = '[c]ontinue' },
     { '<leader>dC', function() require('dap').run_to_cursor() end, desc = 'run to [C]ursor' },
-    { '<leader>dh', function() require('dap.ui.widgets').hover() end, desc = '[h]over', mode = { 'n', 'x' } },
+    { '<leader>dh', focus_or_open_dap_hover, desc = '[h]over', mode = { 'n', 'x' } },
     { '<leader>de', function() require('dapui').eval() end, desc = '[e]val', mode = { 'n', 'x' } },
     { '<leader>di', function() require('dap').step_into() end, desc = 'step [i]nto' },
     { '<leader>d<Down>', function() require('dap').down() end, desc = '[] frame' },
