474833231d26c3157a08a3dd018abfaa90c14e55
Ilya Marchik <ketch.dev@gmail.com>
2026-05-07 14:55:17 +0200

    change flash keys

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -8,34 +8,17 @@ return {
     opts = {},
     keys = {
       {
-        'h',
+        '<C-m>',
         mode = { 'n', 'x', 'o' },
         function() require('flash').jump() end,
-        desc = 'Flash',
+        desc = '[m]ove cursor',
       },
       {
-        'H',
+        '<C-w>',
         mode = { 'n', 'x', 'o' },
         function() require('flash').treesitter() end,
-        desc = 'Flash Treesitter',
+        desc = '[w]raps (treesitter)',
       },
-      -- {
-      --   'r',
-      --   mode = 'o',
-      --   function()
-      --     require('flash').remote()
-      --   end,
-      --   desc = 'Remote Flash',
-      -- },
-      -- {
-      --   'R',
-      --   mode = { 'o', 'x' },
-      --   function()
-      --     require('flash').treesitter_search()
-      --   end,
-      --   desc = 'Treesitter Search',
-      -- },
-      -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
 }
