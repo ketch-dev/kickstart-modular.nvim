4879a3bd9d898b82475b2da8dfbdfe5b935c3dd7
Ilya Marchik <ketch.dev@gmail.com>
2026-05-08 16:42:05 +0200

    [diffview] disable hjkl

--- lua/custom/plugins/diffview/diffview.lua
+++ lua/custom/plugins/diffview/diffview.lua
@@ -1,16 +1,6 @@
 local highlights = require 'custom.plugins.diffview.highlights'
 local open_entry = require 'custom.plugins.diffview.open-entry'
 local open_view = require 'custom.plugins.diffview.open-view'
-local preview_entry = require 'custom.plugins.diffview.preview-entry'
-
-local function fold_action(action)
-  return function()
-    local view = require('diffview.lib').get_current_view()
-    local item = view and view.panel and view.panel:get_item_at_cursor() or nil
-
-    if item and type(item.collapsed) == 'boolean' then action() end
-  end
-end
 
 return {
   {
@@ -21,7 +11,6 @@ return {
       { '<leader>gh', '<cmd>DiffviewFileHistory %<CR>', desc = 'file [h]istory' },
     },
     config = function()
-      local actions = require 'diffview.actions'
       local close = { 'n', '<C-g>', function() require('diffview').close() end, { desc = 'Close Diffview' } }
 
       require('diffview').setup {
@@ -34,9 +23,11 @@ return {
             ['<down>'] = false,
             ['<up>'] = false,
             ['<right>'] = false,
+            { 'n', 'j', function() end, { desc = 'No-op' } },
+            { 'n', 'k', function() end, { desc = 'No-op' } },
+            { 'n', 'h', function() end, { desc = 'No-op' } },
+            { 'n', 'l', function() end, { desc = 'No-op' } },
             close,
-            { 'n', 'j', preview_entry.move_and_preview(actions.next_entry), { desc = 'Next entry + preview commit' } },
-            { 'n', 'k', preview_entry.move_and_preview(actions.prev_entry), { desc = 'Prev entry + preview commit' } },
           },
           option_panel = { close },
           help_panel = { close },
@@ -51,12 +42,12 @@ return {
             ['<up>'] = false,
             ['<right>'] = false,
             ['<cr>'] = false,
-            close,
-            { 'n', 'j', preview_entry.move_and_preview(actions.next_entry), { desc = 'Next entry + preview file' } },
-            { 'n', 'k', preview_entry.move_and_preview(actions.prev_entry), { desc = 'Prev entry + preview file' } },
-            { 'n', 'h', fold_action(actions.close_fold), { desc = 'Collapse folder' } },
-            { 'n', 'l', fold_action(actions.open_fold), { desc = 'Expand folder' } },
+            { 'n', 'j', function() end, { desc = 'No-op' } },
+            { 'n', 'k', function() end, { desc = 'No-op' } },
+            { 'n', 'h', function() end, { desc = 'No-op' } },
+            { 'n', 'l', function() end, { desc = 'No-op' } },
             { 'n', '<cr>', open_entry.open_file_and_close_diffview, { desc = 'Open file and close Diffview' } },
+            close,
           },
         },
       }
