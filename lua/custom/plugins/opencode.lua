ff80e2b31b0f4594a1c17f71193aaa1caab5af45
Ilya Marchik <ketch.dev@gmail.com>
2026-05-14 10:36:38 +0200

    [opencode] switch keymap from [m]essage to [p]rompt

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -13,7 +13,7 @@ return {
       vim.o.autoread = true
       local opencode = require 'opencode'
 
-      vim.keymap.set({ 'n', 'x' }, '<leader>am', function() opencode.ask('@this: ', { submit = true }) end, { desc = '[m]essage' })
+      vim.keymap.set({ 'n', 'x' }, '<leader>ap', function() opencode.ask('@this: ', { submit = true }) end, { desc = '[p]rompt' })
       vim.keymap.set({ 'n', 'x' }, '<leader>aa', function() opencode.select() end, { desc = '[a]ctions' })
       vim.keymap.set({ 'n', 'x' }, '<leader>as', function() return opencode.operator '@this ' end, { desc = '[s]end', expr = true })
     end,
