20792c09a14b796cb5cfb77f97e05ca0c2e78882
Ilya Marchik <ketch.dev@gmail.com>
2026-05-15 16:32:30 +0200

    convert neo-tree to use vim.pack

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,70 +1,65 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
----@module 'lazy'
----@type LazySpec
-return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+local plugins = {
+  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/MunifTanjim/nui.nvim',
+}
+
+if vim.g.have_nerd_font then table.insert(plugins, 'https://github.com/nvim-tree/nvim-web-devicons') end
+
+vim.pack.add(plugins)
+
+vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
+
+require('neo-tree').setup {
+  hide_root_node = true,
+  retain_hidden_root_indent = false,
+  close_if_last_window = true,
+  sources = { 'filesystem', 'buffers', 'git_status' },
+  window = {
+    mappings = {
+      ['<space>'] = 'none',
+
+      ['h'] = 'none',
+      ['j'] = 'none',
+      ['k'] = 'none',
+      ['l'] = 'none',
+
+      ['<left>'] = 'close_node',
+      ['<down>'] = 'move_down',
+      ['<up>'] = 'move_up',
+      ['<right>'] = 'open',
+
+      ['n'] = 'rename',
+      ['y'] = 'copy_to_clipboard',
+      ['s'] = 'open_vsplit',
+      ['K'] = 'expand_all_nodes',
+    },
   },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  ---@module 'neo-tree'
-  ---@type neotree.Config
-  opts = {
-    hide_root_node = true,
-    retain_hidden_root_indent = false,
-    close_if_last_window = true,
-    sources = { 'filesystem', 'buffers', 'git_status' },
-    window = {
-      mappings = {
-        ['<space>'] = 'none',
-
-        ['h'] = 'none',
-        ['j'] = 'none',
-        ['k'] = 'none',
-        ['l'] = 'none',
-
-        ['<left>'] = 'close_node',
-        ['<down>'] = 'move_down',
-        ['<up>'] = 'move_up',
-        ['<right>'] = 'open',
-
-        ['n'] = 'rename',
-        ['y'] = 'copy_to_clipboard',
-        ['s'] = 'open_vsplit',
-        ['K'] = 'expand_all_nodes',
+  filesystem = {
+    hijack_netrw_behavior = 'open_default',
+    group_empty_dirs = true,
+    follow_current_file = {
+      enabled = true,
+      leave_dirs_open = false,
+    },
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_by_name = {
+        'node_modules',
+        '.git',
       },
     },
-    filesystem = {
-      hijack_netrw_behavior = 'open_default',
-      group_empty_dirs = true,
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = false,
-      },
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_by_name = {
-          'node_modules',
-          '.git',
-        },
-      },
-    },
-    default_component_configs = {
-      icon = {
-        folder_closed = '',
-        folder_open = '',
-        folder_empty = '',
-        folder_empty_open = '',
-      },
+  },
+  default_component_configs = {
+    icon = {
+      folder_closed = '',
+      folder_open = '',
+      folder_empty = '',
+      folder_empty_open = '',
     },
   },
 }
