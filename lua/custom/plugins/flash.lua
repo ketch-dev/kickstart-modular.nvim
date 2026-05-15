dd07fa632c97927d7546ed1ea8ef24429737a7f0
Ilya Marchik <ketch.dev@gmail.com>
2026-05-15 14:36:25 +0200

    update flash labels priority

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -1,5 +1,7 @@
 vim.pack.add { 'https://github.com/folke/flash.nvim' }
-require('flash').setup {}
+require('flash').setup {
+  labels = 'shtalegynicfdoruwpmjxvkzqb',
+}
 
 vim.keymap.set({ 'n', 'x', 'o' }, 'j', function() require('flash').jump() end, { desc = '[j]ump' })
 vim.keymap.set({ 'n', 'x', 'o' }, '<C-w>', function() require('flash').treesitter() end, { desc = '[w]raps (treesitter)' })
