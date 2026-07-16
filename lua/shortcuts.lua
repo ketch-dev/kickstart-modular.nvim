61696c5ab3a084765bcf9c2173164c9912dc389b
Ilya Marchik <ketch.dev@gmail.com>
2026-07-16 17:18:39 +0200

    [telescope] use arrows for navigation

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -1,13 +1,15 @@
 return {
-  -- General
+  -- ========== General ==========
   kill_buffer = '<C-k>',
   leave = '<C-l>',
   navigate_up = '<Up>',
   navigate_down = '<Down>',
+  -------------------------------------------------------------------------------
 
-  -- Suggestions
+  -- ========== Suggestions ==========
   trigger_completion = '<C-space>',
   accept_suggestion = '<C-y>',
   cycle_suggestion = '<Tab>',
   cycle_suggestion_backward = '<S-Tab>',
+  -------------------------------------------------------------------------------
 }
