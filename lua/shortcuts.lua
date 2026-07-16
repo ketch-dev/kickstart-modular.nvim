34aad4df1324c086fc38cee8d7e7507b777da834
Ilya Marchik <ketch.dev@gmail.com>
2026-07-16 16:12:58 +0200

    update up and down shortcuts name

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -1,10 +1,13 @@
 return {
+  -- General
   kill_buffer = '<C-k>',
   leave = '<C-l>',
+  navigate_up = '<Up>',
+  navigate_down = '<Down>',
+
+  -- Suggestions
   trigger_completion = '<C-space>',
   accept_suggestion = '<C-y>',
   cycle_suggestion = '<Tab>',
   cycle_suggestion_backward = '<S-Tab>',
-  previous_suggestion = '<Up>',
-  next_suggestion = '<Down>',
 }
